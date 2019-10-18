b8792b6366e96381a36ff5267e7d120ddc0e8e1e
neired <nmillans91@gmail.com>
2019-10-18 18:11:43 +0200

    add types to each pokemon

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -24,20 +24,18 @@ class Pokecard extends React.Component {
     return (
       <div className="pokemon-card">
         <div className="pokemon-card_top-container">
-          {/* <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/` + pokemon.id + `.png`} alt={pokemon.name} className="pokemon-img"></img> */}
           <img src={pokemon.sprites.front_default} alt={pokemon.name} className="pokemon-img"></img>
           <p className="pokemon-id">ID/{pokemon.id}</p>
         </div>
         <div className="pokemon-bottom-container">
           <h2 className="pokemon-name">{pokemon.name}</h2>
           <div className="pokemon-types_container">
-            {/* <p>{pokemonItem.types.type.name}</p> */}
-            {/* <ul className="pokemon-type">
-              {pokemonItem.types
+            <ul className="pokemon-type">
+              {pokemon.types
                 .map((type, index) => { return (
                   <li key={index}>{type.type.name}</li>
               )})}
-            </ul> */}
+            </ul>
           </div>
         </div>
       </div>
