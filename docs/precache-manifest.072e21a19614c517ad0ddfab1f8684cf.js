a3f47202b8968ebc6abd35118f67ad6dd0441edf
neired <nmillans91@gmail.com>
2019-10-21 12:52:41 +0200

    create docs folder

---
+++ docs/precache-manifest.072e21a19614c517ad0ddfab1f8684cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b1e1a88d954354fe16cd73d66492e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ef1e2947ef50a36613fd",
+    "url": "./static/css/main.96c493bf.chunk.css"
+  },
+  {
+    "revision": "4f240be1527d82513303",
+    "url": "./static/js/2.4d174a28.chunk.js"
+  },
+  {
+    "revision": "ef1e2947ef50a36613fd",
+    "url": "./static/js/main.31b69a9e.chunk.js"
+  },
+  {
+    "revision": "24708eecdfb30f7fab46",
+    "url": "./static/js/runtime-main.7c3d63d0.js"
+  },
+  {
+    "revision": "49255cb281084c9c58a3e8c86e743a16",
+    "url": "./static/media/logo.49255cb2.png"
+  }
+]);
