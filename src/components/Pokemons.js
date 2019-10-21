ada3e3755155a9e81ea74f4e59c8a0d0cd28b7e9
neired <nmillans91@gmail.com>
2019-10-21 11:58:35 +0200

    add no-pokemons message

--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -5,6 +5,11 @@ import './../styles/pokemons.scss';
 const Pokemons = props => {
   const {pokemons, userInput} = props;
 
+  if (pokemons.filter(pokemon => pokemon.name.toUpperCase().includes(userInput.toUpperCase())).length < 1) {
+    return (
+    <p>Ups! No hay pokemons con ese nombre</p>
+    );
+  } else {
   return (
     <ol className="pokelist">
       {pokemons
@@ -19,7 +24,7 @@ const Pokemons = props => {
           </li>
         )})}
     </ol>
-  );
+  );}
 };
 
 export default Pokemons;
